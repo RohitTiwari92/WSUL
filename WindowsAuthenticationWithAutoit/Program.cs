8a1a7f91ddb8e46aadede19433ff3b987754c749
Rohit Tiwari <rohit.tiwari@logisure.in>
2017-12-02 00:29:50 +0530

    DemoApp

---
+++ WindowsAuthenticationWithAutoit/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.IE;
+using AutoIt;
+using System.Threading;
+
+namespace WindowsAuthenticationWithAutoit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new InternetExplorerDriver();
+            driver.Navigate().GoToUrl("http://localhost/Home/StephenSecrets");
+            Thread.Sleep(500); //if required 
+            AutoItX.WinWaitActive("Windows Security");
+            AutoItX.ControlSetText("Windows Security", "", "Edit1", "TestUser");
+            AutoItX.ControlSetText("Windows Security", "", "Edit2", "Password_test");
+            AutoItX.Send("{ENTER}");
+        }
+    }
+}
